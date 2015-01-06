8e4cf889f5fd8157a9472e80810ffa9f3137a2a2
Yuki Kobayashi <yuki10.kb@outlook.com>
2015-01-06 23:02:40 +0900

    update for JBoss EAP6

--- src/main/java/org/seasar/framework/util/VFSResourcesFactory.java
+++ src/main/java/org/seasar/framework/util/VFSResourcesFactory.java
@@ -20,7 +20,7 @@ import org.seasar.framework.util.ResourcesUtil.ResourcesFactory;
 
 /**
  * ResourcesFactory for JBossAS 7 vfs.
- * 
+ *
  * @author wadahiro
  */
 public class VFSResourcesFactory implements ResourcesFactory {
@@ -39,15 +39,23 @@ public class VFSResourcesFactory implements ResourcesFactory {
             URI physicalURI = VFSUtils.getPhysicalURI(vf);
 
             debugPhysicalURI(physicalURI);
-            
-            if (url.getPath().toLowerCase().matches(".*¥.jar/.*")) {
-            
-                File file = new File(url.getPath());
-                if (file.isDirectory()) {
-                    return handleDir(physicalURI, rootPackage, rootDir);
+            if (url.getPath().toLowerCase().matches(".*\\.jar.*")) {
+
+                int afterJarIndex = 0;
+
+                if (null == rootPackage || "".equals(rootPackage)) {
+                    afterJarIndex = url.getPath().lastIndexOf("/");
                 } else {
-                    return handleJar(physicalURI, rootPackage, rootDir);
+                    afterJarIndex = url.getPath().toLowerCase().indexOf("/" + rootPackage.replaceAll("\\.", "/"));
                 }
+
+                String jarFilePath = url.getPath().substring(0, afterJarIndex);
+                File file = new File(jarFilePath);
+                String jarFileName = file.getName();
+
+                //.jarを含む場合、jarをハンドリングする
+                return handleJar(jarFileName, physicalURI, rootPackage, rootDir);
+
             } else {
                 return handleDir(physicalURI, rootPackage, rootDir);
             }
@@ -58,35 +66,71 @@ public class VFSResourcesFactory implements ResourcesFactory {
         }
     }
 
+    /**
+     * ディレクトリリソースをハンドリングします。
+     *
+     * @param physicalURI physicalURI
+     * @param rootPackage rootPackage
+     * @param rootDir rootDir
+     * @return Resource
+     * @throws MalformedURLException
+     */
     private Resources handleDir(URI physicalURI, final String rootPackage,
             final String rootDir) throws MalformedURLException {
         return new FileSystemResources(ResourcesUtil.getBaseDir(
                 physicalURI.toURL(), rootDir), rootPackage, rootDir);
     }
 
-    private Resources handleJar(URI physicalURI, final String rootPackage,
+    /**
+     * Jarファイルリソースをハンドリングします。
+     *
+     * @param jarFileName jarFileName
+     * @param physicalURI physicalURI
+     * @param rootPackage rootPackage
+     * @param rootDir rootDir
+     * @return Resource
+     * @throws IOException
+     */
+    private Resources handleJar(String jarFileName, URI physicalURI, final String rootPackage,
             final String rootDir) throws IOException {
-        File f = new File(physicalURI);
+
+
+        String physicalURIStr = physicalURI.getPath();
+        physicalURIStr = physicalURIStr.replaceAll("file:", "");
+        int strIndex = physicalURIStr.lastIndexOf("/contents");
+        physicalURIStr = physicalURIStr.substring(0, strIndex + 1);
+        physicalURIStr += jarFileName;
+
+
+        File f = new File(physicalURIStr);
+
         File[] jar = f.getParentFile().listFiles(new FileFilter() {
             public boolean accept(File pathname) {
-//                return pathname.getName().toLowerCase().endsWith(".jar");
-            	return pathname.getName().toLowerCase().matches(".*¥.jar/.*");
+                return pathname.getName().toLowerCase().endsWith(".jar");
             }
         });
 
         debugJarPath(jar);
-
         JarFile jarFile = new JarFile(jar[0]);
-
         return new JarFileResources(jarFile, rootPackage, rootDir);
     }
 
+    /**
+     * PhysicalURI情報をデバッグログ出力します。
+     *
+     * @param physicalURI PhysicalURI
+     */
     private void debugPhysicalURI(URI physicalURI) {
         if (logger.isDebugEnabled()) {
             logger.debug("Converted physicalURI:" + physicalURI);
         }
     }
 
+    /**
+     * Jarファイルパスをデバッグログ出力します。
+     *
+     * @param jarFile jarFile
+     */
     private void debugJarPath(File[] jarFile) {
         if (logger.isDebugEnabled()) {
             for (File file : jarFile) {
@@ -95,6 +139,13 @@ public class VFSResourcesFactory implements ResourcesFactory {
         }
     }
 
+    /**
+     * 引数の情報をデバッグログ出力します。
+     *
+     * @param url URL
+     * @param rootPackage RootPackage
+     * @param rootDir RootDir
+     */
     private void debugArgs(URL url, String rootPackage, String rootDir) {
         if (logger.isDebugEnabled()) {
             logger.debug("create resource: dump args...");
